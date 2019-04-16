8a7e8c8c545672ddf1785cb395957bd998235f09
Xia Dong <dongxiapp@gmail.com>
2019-04-16 01:51:30 +0000

    announce the winner at the end of a game

--- main.rb
+++ main.rb
@@ -9,15 +9,17 @@ class Game
     puts '-------- NEW TURN --------'
     if @currentPlayer === @player1
       @currentPlayer = @player2
+      other = @player1
     else
       @currentPlayer = @player1
+      other = @player2
     end
     q1 = Question.new
     puts "#{@currentPlayer.name}: #{q1.askQuestion}"
     checkResult = q1.checkAnswer
     puts checkResult ? ("#{@currentPlayer.name}: Yes! You are correct") : ("#{@currentPlayer.name}: Seriously? No!")
     @currentPlayer.lives -= 1 unless checkResult
-    puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
+    puts @currentPlayer.lives === 0 ? ("-------- GAME OVER --------\n#{other.name} wins with a score of #{other.lives}/3" ): "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
   end
 
   def newGame 
