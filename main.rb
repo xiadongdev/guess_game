d29cb20e538aba9c451980229b756e410ec2084f
Xia Dong <dongxiapp@gmail.com>
2019-04-16 01:41:16 +0000

    implement number guessing name

--- main.rb
+++ main.rb
@@ -1,47 +1,56 @@
 class Game
   def initialize
-    @player1 = Player.new
-    @player2 = Player.new
+    @player1 = Player.new 'Alice'
+    @player2 = Player.new 'Bob'
   end
 
   def newTurn
     #switch currentPlayer
-    mathQ
-    #check currentPlayer input
-    #currentPlayer.lives - 1
-    #puts player1.lives, player2.lives
+    puts '-------- NEW TURN --------'
+    if @currentPlayer === @player1
+      @currentPlayer = @player2
+    else
+      @currentPlayer = @player1
+    end
+    q1 = Question.new
+    puts "#{@currentPlayer.name}: #{q1.askQuestion}"
+    checkResult = q1.checkAnswer
+    puts checkResult ? ("#{@currentPlayer.name}: Yes! You are correct") : ("#{@currentPlayer.name}: Seriously? No!")
+    @currentPlayer.lives -= 1 unless checkResult
+    puts "#{@player1.name}: #{@player1.lives}/3 vs #{@player2.name}: #{@player2.lives}/3"
   end
 
-  def newGame
-    currentPlayer = @player1
-    newTurn
-    #call newTurn until player.lives = 0
+  def newGame 
+    @currentPlayer = @player1
+    while @currentPlayer.lives > 0 do
+      newTurn
+    end
   end
 end
 
 class Player
-  attr_accessor :lives
-  def initialize
+  attr_accessor :lives, :name
+  def initialize name
     @lives = 3
+    @name = name
   end
 end
 
 class Question
-  def answer
-    @answer = @rand1 + @rand2
-  end
-  
-  def initilize
+  def initialize
     @rand1 = rand(1..20)
     @rand2 = rand(1..20)
   end
 
   def askQuestion
-    puts 'What does #{rand1} plus #{rand2} equal? '
+    "What does #{@rand1} plus #{@rand2} equal?"
   end
 
-  def checkAnswer number
-    number === @answer ? true : false
+  def checkAnswer
+    gets.chomp.to_i === @rand1 + @rand2 ? true : false
   end
 
 end
+
+game = Game.new
+game.newGame
